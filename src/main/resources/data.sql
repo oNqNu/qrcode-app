6caa2aecda7553dfd8507aa63fc6a8198e155536
oNqNu <onanukintaro@gmail.com>
2022-08-18 18:25:58 +0900

    feat: schemaとdataを作成

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+-- 従業員テーブルのデータ
+INSERT INTO employee(
+    employee_id,
+    employee_name,
+    age
+)
+VALUES(1,'山田太郎',30);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+-- 従業員テーブル
+CREATE TABLE IF NOT EXISTS employee (
+    employee_id INT PRIMARY KEY,
+    employee_name VARCHAR(50),
+    age INT
+);
+
