2e0294a4d4095132c86836216d29b33940777d64
oNqNu <onanukintaro@gmail.com>
2022-08-18 17:15:36 +0900

    init: qrcode-app-backend

---
+++ src/main/java/com/qrcode/qrcodeapp/QrcodeAppApplication.java
@@ -0,0 +1,13 @@
+package com.qrcode.qrcodeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qrcode/qrcodeapp/QrcodeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qrcode.qrcodeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
