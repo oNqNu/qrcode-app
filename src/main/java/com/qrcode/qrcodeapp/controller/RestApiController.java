d00bf74cb9a84b3f0f0c85b9b66dc0194e5b959d
oNqNu <onanukintaro@gmail.com>
2022-08-19 18:17:47 +0900

    feat: urlパラメータ使えるようにした

--- src/main/java/com/qrcode/qrcodeapp/controller/RestApiController.java
+++ src/main/java/com/qrcode/qrcodeapp/controller/RestApiController.java
@@ -1,6 +1,7 @@
 package com.qrcode.qrcodeapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestApiController {
  
     @RequestMapping("hello")
-    private String hello() {
-        return "SpringBoot!";
+    private String hello(@RequestParam("test") String str) {
+        return str;
     }
 
 }
