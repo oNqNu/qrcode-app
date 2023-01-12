b3fef568dcb6b2fa5d3a36c19efb85096458ef43
oNqNu <onanukintaro@gmail.com>
2023-01-12 13:51:45 +0900

    feat: サンプルを返すAPI実装

--- src/main/java/com/qrcode/qrcodeapp/controller/RestApiController.java
+++ src/main/java/com/qrcode/qrcodeapp/controller/RestApiController.java
@@ -34,6 +34,12 @@ public class RestApiController {
         return str;
     }
 
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    public String test() {
+
+        return "helloWorld";
+    }
+
     @RequestMapping(value = "post_test",method = RequestMethod.POST)
     private String post_test(@RequestBody OriginInputItem item) {
         System.out.println(item.getMask_pattern());
@@ -94,10 +100,62 @@ public class RestApiController {
         String str8 = item.getVariance();
         String str9 = item.getY_axis();
         String str10 = item.getX_axis();
-
+        
         String base64data = QRmain.execute(new String[] {str_path,str_data,str1,str2,str3,str4,str5,str6,str7,str8,str9,str10});
+        System.out.println(base64data);
 
         Path p = Paths.get("src/main/resources/img/input/tttt.jpg");
+
+        // try{
+        // Files.deleteIfExists(p);
+        // }catch(IOException e){
+        // System.out.println(e);
+        // }
+        
+        return base64data;
+        // return "aa";
+    }
+
+
+    @RequestMapping(value = "get_sample",method = RequestMethod.GET)
+    private String get_sample() {
+
+
+        // // String str_path = "src/main/resources/img/abe.jpg";
+        // String img_string = item.getImg_string();
+        // int index = img_string.indexOf(",");
+        // String result = img_string.substring(index + 1);
+        // System.out.println(result.substring(0, 3));
+
+        // try{
+        //     byte[] bytes = Base64.getDecoder().decode(result.getBytes());
+        //     ByteArrayInputStream input = new ByteArrayInputStream(bytes);
+        //     BufferedImage image = ImageIO.read(input);
+        //     FileOutputStream output =
+        //     new FileOutputStream("src/main/resources/img/input/tttt.jpg");
+        //     ImageIO.write(image, "jpg", output);
+        //     }catch(IOException e){
+        //         System.out.println(e.toString());
+        //         System.out.println("なんかおかしいこと起きてんで");
+        //     }
+        
+        String str_path = "src/main/resources/img/kobe.jpg";
+        // String str_path = "src/main/resources/img/input/tttt.jpg";
+        String str_data = "https://www.okayama-u.ac.jp";
+        String str1 = "5";
+        String str2 = "0";
+        String str3 = "0";
+        String str4 = "0";
+        String str5 = "100";
+        String str6 = "96";
+        String str7 = "50";
+        String str8 = "0.7";
+        String str9 = "0.5";
+        String str10 = "0.5";
+        
+        String base64data = QRmain.execute(new String[] {str_path,str_data,str1,str2,str3,str4,str5,str6,str7,str8,str9,str10});
+        System.out.println(base64data);
+
 
         // try{
         // Files.deleteIfExists(p);
