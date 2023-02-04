64783ece535874476561b8c54e6b238f5bcfad7e
oNqNu <onanukintaro@gmail.com>
2023-02-04 12:37:17 +0900

    bodyをMapで受け取る用のAPI作成

--- src/main/java/com/qrcode/qrcodeapp/controller/RestApiController.java
+++ src/main/java/com/qrcode/qrcodeapp/controller/RestApiController.java
@@ -146,6 +146,65 @@ public class RestApiController {
         return QRmain.execute(new String[] {str_path,str_data,str1,str2,str3,str4,str5,str6,str7,str8,str9,str10});
     }
 
+    @RequestMapping(value = "create_qrcode_bymap",method = RequestMethod.POST)
+    private Map<String,String> create_qrcode_bymap(@RequestBody Map<String,String> item) {
+
+	    System.out.println("最初");
+        long total = Runtime.getRuntime().totalMemory();
+	    long free = Runtime.getRuntime().freeMemory();
+	    long max = Runtime.getRuntime().maxMemory();
+	    System.out.println("total: " + total / 1024 + "kb");
+	    System.out.println("free: " + free / 1024 + "kb");
+	    System.out.println("max: " + max / 1024 + "kb");
+
+        String img_string = item.get("img_string");
+        int index = img_string.indexOf(",");
+        String result = img_string.substring(index + 1);
+        img_string = null;
+
+        try{
+            // byte[] bytes = Base64.getDecoder().decode(result.getBytes());
+            ByteArrayInputStream input = new ByteArrayInputStream(Base64.getDecoder().decode(result.getBytes()));
+            BufferedImage image = ImageIO.read(input);
+            input = null;
+            FileOutputStream output =
+            new FileOutputStream("src/main/resources/img/input/tttt.jpg");
+            ImageIO.write(image, "jpg", output);
+            output = null;
+            }catch(IOException e){
+                System.out.println(e.toString());
+                System.out.println("なんかおかしいこと起きてんで");
+            }
+
+        result = null;
+        
+        // String str_path = "src/main/resources/img/kobe.jpg";
+        String str_path = "src/main/resources/img/input/tttt.jpg";
+        String str_data = item.get("data");
+        String str1 = item.get("version");
+        String str2 = item.get("cee_level");
+        String str3 = item.get("encoding");
+        String str4 = item.get("mask_pattern");
+        String str5 = item.get("trial_times");
+        String str6 = item.get("threshold");
+        String str7 = item.get("scale");
+        String str8 = item.get("variance");
+        String str9 = item.get("y_axis");
+        String str10 = item.get("x_axis");
+
+        item = null;
+        System.gc();
+
+        long atotal = Runtime.getRuntime().totalMemory();
+	    long afree = Runtime.getRuntime().freeMemory();
+	    long amax = Runtime.getRuntime().maxMemory();
+	    System.out.println("total: " + atotal / 1024 + "kb");
+	    System.out.println("free: " + afree / 1024 + "kb");
+	    System.out.println("max: " + amax / 1024 + "kb");
+        
+        return QRmain.execute(new String[] {str_path,str_data,str1,str2,str3,str4,str5,str6,str7,str8,str9,str10});
+    }
+
     @RequestMapping(value = "post_test",method = RequestMethod.POST)
     private Map<String,String> post_test(@RequestBody OriginInputItem item) {
 
